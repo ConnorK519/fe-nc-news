d16f1b23b274a371283e7b589fbb6ae7a6c4b979
connork <connorkelly-95@hotmail.co.uk>
2023-07-18 14:03:45 +0100

    adds links to articles page

--- src/components/ArticleInfo.jsx
+++ src/components/ArticleInfo.jsx
@@ -1,12 +1,15 @@
 import "../App.css";
 import dayjs from "dayjs";
+import { Link } from "react-router-dom";
 
 export const ArticleInfo = ({ article }) => {
   const written = dayjs(article.created_at).format("dddd, MMMM D, YYYY");
   return (
     <>
       <section className="articleInfo">
-        <h2 className="title">{article.title}</h2>
+        <Link className="link" to={`/articles/${article.article_id}`}>
+          <h2 className="title">{article.title}</h2>
+        </Link>
         <h3>Topic: {article.topic}</h3>
         <p className="author"> Author: {article.author}</p>
         <p className="dateMade">Written: {written}</p>
