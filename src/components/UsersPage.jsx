30b6c11e357f42d112903a84a2a33f9076e486c5
connork <connorkelly-95@hotmail.co.uk>
2023-07-22 01:23:27 +0100

    trying to fix the context

--- src/components/UsersPage.jsx
+++ src/components/UsersPage.jsx
@@ -1,10 +1,12 @@
 import { useState, useEffect } from "react";
 import { getUsers } from "../api";
-import { UserCard } from "./UserCard";
 import "../App.css";
+import { useContext } from "react";
+import { UserContext } from "../contexts/UserContext";
 
 export const UsersPage = () => {
   const [users, setUsers] = useState([]);
+  const { currentUser, setCurrentUser } = useContext(UserContext);
 
   useEffect(() => {
     getUsers().then(({ users }) => {
@@ -16,7 +18,22 @@ export const UsersPage = () => {
     <>
       <section>
         {users.map((user, index) => {
-          return <UserCard key={index} user={user} />;
+          return (
+            <div
+              key={index}
+              onClick={() => {
+                setCurrentUser(user.username);
+              }}
+              className="userCard"
+            >
+              <img
+                key={user.avatar_url}
+                className="avatarSize"
+                src={user.avatar_url}
+              />
+              <h3 key={user.username}>{user.username}</h3>
+            </div>
+          );
         })}
       </section>
     </>
